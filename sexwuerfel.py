67b5be66d0ffac6cf68b55ab5e43c38db7704755
Fiona Krakenbuerger <fotografiona@gmail.com>
2013-04-06 00:49:09 +0200

    erste version hinzufuegen

---
+++ sexwuerfel.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+print "L I E B E S W U E R F E L"
+
+# wie = ("zaertlich", "fest", "brutal", "schlecht", "angewidert", "lustig")
+wo = ("Nippel", "PENIS", "Pussy", "Schenkel", "Bauch", "Po")
+was = ("kuessen", "saugen", "lecken", "knabbern", "streicheln", "kitzeln") 
+
+
+from random import choice
+# print choice (wie) 
+print choice (wo)
+print choice (was) 
+
+
+
+
