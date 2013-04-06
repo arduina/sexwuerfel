8eccadbe6478effd0051bad60c9a6f81eeb9b1d6
Matthias Bauer <moeffju@moeffju.net>
2013-04-06 20:44:33 +0200

    Encoding-Marker hinzufügen für Umlaute

--- sexwuerfel.py
+++ sexwuerfel.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 from random import choice
 
-# wie = ("zaertlich", "fest", "brutal", "schlecht", "angewidert", "lustig")
+# wie = ("zärtlich", "fest", "brutal", "schlecht", "angewidert", "lustig")
 wo = ("Nippel", "PENIS", "Pussy", "Schenkel", "Bauch", "Po")
-was = ("kuessen", "saugen", "lecken", "knabbern", "streicheln", "kitzeln")
+was = ("küssen", "saugen", "lecken", "knabbern", "streicheln", "kitzeln")
 
-print "L I E B E S W U E R F E L"
+print "L I E B E S W Ü R F E L"
 
-# print choice (wie)
+# print choice(wie)
 print choice(wo)
 print choice(was)
