3abe36be38588d77dba70f14dedc8c2d6a3bb5f2
Matthias Bauer <moeffju@moeffju.net>
2013-04-06 20:46:14 +0200

    Mehr „wie“

--- sexwuerfel.py
+++ sexwuerfel.py
@@ -3,12 +3,12 @@
 
 from random import choice
 
-# wie = ("zärtlich", "fest", "brutal", "schlecht", "angewidert", "lustig")
+wie = ("zärtlich", "fest", "brutal", "schlecht", "angewidert", "lustig")
 wo = ("Nippel", "PENIS", "Pussy", "Schenkel", "Bauch", "Po")
 was = ("küssen", "saugen", "lecken", "knabbern", "streicheln", "kitzeln")
 
 print "L I E B E S W Ü R F E L"
 
-# print choice(wie)
+print choice(wie)
 print choice(wo)
 print choice(was)
