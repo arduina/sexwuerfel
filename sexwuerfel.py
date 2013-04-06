09a8e6cfc7c41e941e44608bc0b003f9af76c7d4
Matthias Bauer <moeffju@moeffju.net>
2013-04-06 20:43:53 +0200

    Reformatierung nach PEP8

--- sexwuerfel.py
+++ sexwuerfel.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
-print "L I E B E S W U E R F E L"
+
+from random import choice
 
 # wie = ("zaertlich", "fest", "brutal", "schlecht", "angewidert", "lustig")
 wo = ("Nippel", "PENIS", "Pussy", "Schenkel", "Bauch", "Po")
-was = ("kuessen", "saugen", "lecken", "knabbern", "streicheln", "kitzeln") 
+was = ("kuessen", "saugen", "lecken", "knabbern", "streicheln", "kitzeln")
 
+print "L I E B E S W U E R F E L"
 
-from random import choice
-# print choice (wie) 
-print choice (wo)
-print choice (was) 
-
-
-
-
+# print choice (wie)
+print choice(wo)
+print choice(was)
