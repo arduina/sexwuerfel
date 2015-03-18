1ea5509bd5df044809f218530bed243299a64623
Christian Lauf <ChrLau@users.noreply.github.com>
2015-03-18 16:03:43 +0100

    Update sexwuerfel.py
    Mal die _wirklich_ interessanten Körperteile und Orte ergänzt :-P

--- sexwuerfel.py
+++ sexwuerfel.py
@@ -3,11 +3,11 @@
 
 from random import choice
 
-wer = ("ich", "Du", "das aufblasbare Schaf", "deine Mutter")
+wer = ("ich", "Du", "das aufblasbare Schaf", "deine Mutter", "sie")
 wie = ("zärtlich", "fest", "brutal", "schlecht", "angewidert", "lustig")
-wo = ("Nippel", "Penis", "Pussy", "Schenkel", "Bauch", "Po")
+wo = ("Nippel", "Penis", "Pussy", "Schenkel", "Bauch", "Po", "Bauchnabel", "Ohrläppchen", "Nacken", "Hals", "kleiner Zeh")
 was = ("küssen", "saugen", "lecken", "knabbern", "streicheln", "kitzeln", "schlagen", "foltern")
-ort = ("in der Küche", "im Wohnzimmer", "im Aufzug", "im Bürgeramt", "auf dem Klo", "im Bett", "draußen")
+ort = ("in der Küche", "im Wohnzimmer", "im Aufzug", "im Bürgeramt", "auf dem Klo", "im Bett", "draußen", "im Türrahmen", "an den Dachbalken", "im Flur", "auf dem Balkon")
 mod = ("Los!", "Zehn Minuten!", "Juhu!", "Okay.")
 
 print "L I E B E S W Ü R F E L\n"
